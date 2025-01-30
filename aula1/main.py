46b1fa104ec54cba338592d04fca12de5756115b
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-01-30 17:18:11 -0300

    aaaaaaa

--- aula1/main.py
+++ aula1/main.py
@@ -65,3 +65,66 @@ df.plot.scatter(x = 'latitude', y = 'longitude', s = 1)
 # textos que o label encoder não conhecem são mais difíceis
 # o fit ensina pro label encoder como ele deve codificar o dado
 # graficos lienares mostgram que duas informações talvez não sejam tão importantes, se houver relação é possível ficar apenas com uma
+
+"""
+
+
+
+# season (1:springer, 2:summer, 3:fall, 4:winter)
+# demorou 10 anos para voltarem a emprestar bikes?
+
+# df.plot.scatter(x = 'hr', y = 'cnt', s = 1)
+
+# quanto mais humido mais bikes
+# quanto mais vento menos bikes
+# quanto mais a estação é fria mais bikes
+
+
+
+# for i in df['season'] :
+#   if i != 1 :
+#     print(i)
+
+# df
+# # obtendo o nosso objetivo
+# Y = df['median_house_value']
+# # removendo o objetivo do resto dos dados
+# X = df.drop('median_house_value', axis=1)
+# # test_size = proporção que vai para teste
+# # random_state = semente aleatória para embaralhar os dados
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=42)
+
+# model = DecisionTreeClassifier(
+#     criterion = "entropy",
+#     max_depth = 20,
+#     min_samples_split = 10
+# )
+
+# model.fit(X_train, Y_train)
+
+# model = DecisionTreeRegressor(
+#     max_depth = 20,
+#     min_samples_split = 10
+# )
+
+# # Criando e treinando o modelo
+# # model = DecisionTreeRegressor()
+# model.fit(X_train, Y_train)
+# # salva o modelo treinado para uso posterior
+# dump(model, 'filename.joblib')
+# # model = load('filename.joblib') # carrega o modelo, evidentemente não é necessário
+
+# Y_real = Y_train
+# Y_pred = model.predict(X_train)
+# train_error = mean_absolute_error(Y_real, Y_pred)
+# Y_real = Y_test
+# Y_pred = model.predict(X_test)
+# test_error = mean_absolute_error(Y_real, Y_pred)
+# print(train_error, test_error) # 50786 50922
+
+# # ele printa o erro, nesse caso eu errei 16 mil dolares
+# vendo cada item das de weekday das 10 primerias colunas
+# for i in primeiros_10:
+#     if 'weekday' in i:
+#         print(i['weekday'])
+"""
